d00973ca31f9debbd391a7af6dd26c9b29cdf3f0
Glendy Tuyuc <glendytuyuc@gmail.com>
2025-05-28 14:38:33 -0600

    adding the 3rd model and metrics with sentry

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,31 @@
-import React,{ StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
+import * as Sentry from "@sentry/react";
 
-createRoot(document.getElementById('root')).render(
+Sentry.init({
+  dsn: "https://55af690e1c4517d9746b51fcbb990408@o4509402516553728.ingest.us.sentry.io/4509402517536768",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    //Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
